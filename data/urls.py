c6e685c758b0040bf7fe439ea0efadf4f65ba0b8
MixaTorres7 <mike.volk107@yandex.ru>
2026-02-21 16:44:33 +0300

    Delete data directory

--- data/urls.py
+++
@@ -1,5 +0,0 @@
-class Urls:
-    url_main = 'https://stellarburgers.education-services.ru/'
-    url_login = 'https://stellarburgers.education-services.ru/login'
-    url_register = 'https://stellarburgers.education-services.ru/register'
-    url_feed = 'https://stellarburgers.education-services.ru/feed'
--- data/user_data.py
+++
@@ -1,6 +0,0 @@
-def get_user_for_register():
-    return {
-        'email': 'MikhailVolkov2026@mail.ru',
-        'password': 'MikhailVolkov2026',
-        'name': 'Mikhail',
-    }
