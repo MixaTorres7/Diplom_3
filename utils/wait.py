1edb35387e11ccc18d3f594199eecd868679577f
MixaTorres7 <mike.volk107@yandex.ru>
2026-02-26 22:11:32 +0300

    delete try except and edit logic

--- utils/wait.py
+++ utils/wait.py
@@ -2,14 +2,6 @@ import time
 
 
 def wait_until(driver, timeout, condition):
-    """Ожидание: проверяет condition каждые 0.5 сек до timeout."""
-    end = time.time() + timeout
-    while time.time() < end:
-        try:
-            result = condition(driver)
-            if result:
-                return result
-        except Exception:
-            pass
-        time.sleep(0.5)
-    raise TimeoutError(f'ожидание {timeout} сек истекло')
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    return WebDriverWait(driver, timeout).until(condition)
